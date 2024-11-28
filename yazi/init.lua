6f21ac2db90faa5644eb75af350287f4ce1d7e04
Filip Lobpreis <f.lobpreis@gmail.com>
2024-11-28 10:12:05 +0100

    yazi: init
    * Installation and usage can be found here:
    https://yazi-rs.github.io/docs/installation/
    
     To install this on Ubuntu you need cargo to be installed.
    
    * To install the packages enter command:
      ya pack --install
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

---
+++ yazi/init.lua
@@ -0,0 +1,16 @@
+-- ~/.config/yazi/init.lua
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	local perms = self._file.cha:permissions()
+	return ui.Line(string.format("%s %s %s", size and ya.readable_size(size) or "-", perms or "-" , time))
+end
