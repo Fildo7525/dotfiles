c23fae58622f6c673232155f3464c438253333ed
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-12 22:43:33 +0100

    swap: set to 8GB
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

---
+++ swap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Turn swap off
+# This moves stuff in swap to the main memory and might take several minutes
+sudo swapoff -a
+
+# Create an empty swapfile
+# Note that "1M" is basically just the unit and count is an integer.
+# Together, they define the size. In this case 8GiB.
+sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
+
+# Set the correct permissions
+sudo chmod 0600 /swapfile
+
+sudo mkswap /swapfile  # Set up a Linux swap area
+sudo swapon /swapfile  # Turn the swap on
