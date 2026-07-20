64495ab0904c3265c4bbadb25ade11b4bb0fc5aa
Filip Lobpreis <f.lobpreis@gmail.com>
2026-07-20 17:40:09 +0200

    hypr: kdeconnect on startup
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -39,6 +39,9 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("~/.config/hypr/scripts/batter_notifier")
 	hl.exec_cmd("wl-paste --type text --watch cliphist store") -- Stores only text data
 	hl.exec_cmd("wl-paste --type image --watch cliphist store") -- Stores only image data
+
+	hl.exec_cmd("kdeconnectd")
+	hl.exec_cmd("kdeconnect-indicator")
 end)
 
 -- #############################
